b71e9ec47ba5384ca05f0f6b621173100a984c07
Dhwaj Sharma <dhwajsharma10@gmail.com>
2023-09-22 20:45:09 +0530

    setup redux store && hider sidebar feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import store from "./utils/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="">
-      <Header />
-      <Body />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+        <Body />
+      </div>
+    </Provider>
   );
 }
 
